bcf9f15fe21cbad4e197948c8784a457ab104c44
Darshan <gaikwaddarshan0111@gmail.com>
2026-06-25 00:28:00 +0530

    PUT Resolved

--- app/main.py
+++ app/main.py
@@ -1,4 +1,4 @@
-from fastapi import Depends, FastAPI
+from fastapi import Depends, FastAPI , HTTPException
 from sqlalchemy.orm import Session
 
 from app.database import Base, engine
@@ -40,9 +40,37 @@ def create_student(student: StudentCreate, db: Session = Depends(get_db)):
 
 @app.get("/students/{student_id}")
 def get_students(student_id: int ,db: Session = Depends(get_db)):
-    students = (
+    student = (
         db.query(models.Student)
         .filter(models.Student.id == student_id)
         .first()
     )
-    return students
+
+    if student is None:
+        raise HTTPException(
+            status_code = 404,
+            detail = "Student not found"
+        )
+    return student
+
+@app.put("/students/{student_id}")
+def update_student(student_id: int ,student: StudentCreate, db: Session = Depends(get_db)):
+    db_student = (
+        db.query(models.Student)
+        .filter(models.Student.id == student_id)
+        .first()
+    )
+    if db_student is None:
+        raise HTTPException(
+            status_code = 404,
+            detail = "Student not found"
+        )
+    
+    db_student.name = student.name
+    db_student.age = student.age
+    db_student.course = student.course
+
+    db.commit()
+    db.refresh(db_student)
+    
+    return db_student
